1b51250264ab7aca63a51ee122506695f168933f
Efreitor2001 <pokimoha.123@gmail.com>
2022-04-22 16:31:07 +0300

    Done

---
+++ Challenge_001/Program.cs
@@ -0,0 +1,21 @@
+﻿void Pal(int a)
+{
+    int b = a;
+    int c = 0;
+    while (b != 0)
+    {
+        c = c * 10 + (b % 10);
+        b /= 10;
+    }
+    if (a == c)
+    {
+        Console.WriteLine("is Pal");
+    }
+    else
+    {
+        Console.WriteLine("is NOT Pal");
+    }
+}
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Pal(num);
